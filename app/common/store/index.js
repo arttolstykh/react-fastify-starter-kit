26e7257f65447bfca3ade77e76c0c45d960eff66
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-11-26 13:20:26 +0200

    wip

--- app/common/store/index.js
+++ app/common/store/index.js
@@ -2,14 +2,15 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from '../reducers'
 
+const dev = process.env.NODE_ENV === 'development'
+
 export default function configureStore (initialState = {}) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose
-
-  const enhancer = composeEnhancers(
-    applyMiddleware(logger)
-  )
-
+  let composeEnhancers
+  
+  if (dev) composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose
+  else composeEnhancers = compose
+  
+  const enhancer = composeEnhancers(applyMiddleware(logger))
+  
   return createStore(rootReducer, initialState, enhancer)
 }
